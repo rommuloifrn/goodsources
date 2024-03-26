e97caa9b717f7ce6f5e8263186ec75765427bab4
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-03-26 08:52:46 -0300

    first commit

---
+++ src/main/java/com/romm/goodsources/GoodsourcesApplication.java
@@ -0,0 +1,13 @@
+package com.romm.goodsources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romm/goodsources/GoodsourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romm.goodsources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
